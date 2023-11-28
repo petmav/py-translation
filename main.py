79762f5e5fc5c32e5cb89402d904cc2cda83293e
4skinny <120860603+4skinny@users.noreply.github.com>
2023-11-28 14:22:46 +1100

    add match statement

--- main.py
+++ main.py
@@ -12,103 +12,98 @@ languages_accepted = ("af am ar as az ba be bg bn bo br bs ca cs cy da de el en 
                       "ur uz vi yi yo zh yue").split()
 
 while True:
-    print(f'Welcome to the py-translation tool!\n'
-          f'Type [help] to see instructions on how to use the program.\n'
-          f'Type [tiny/base/small/medium/large] to choose a model.\n'
-          f'You can type [exit] to cancel the program.\n')
+    model_selection = input(f'Welcome to the py-translation tool!\n'
+                            f'Type [help] to see instructions on how to use the program.\n'
+                            f'Type [tiny/base/small/medium/large] to choose a model.\n'
+                            f'You can type [exit] to cancel the program.\n').lower()
 
-    model_selection = input()
 
-    if model_selection == 'exit':
-        sys.exit()
+    match model_selection:
+        case "exit":
+            sys.exit()
+        case "help":
+            print('This program is utilised to subtitle a video through the use of the OpenAI Whisper model.\n'
+            'To use this model, type [tiny/base/small/medium/large] to choose a model and initiate the process.\n'
+            'After this, you will need the file (mp3/flac/wav extensions accepted) and the language being '
+            'translated.\n')
+            time.sleep(3)
+        case "tiny" | "base" | "small" | "medium" | "large":
+            x = True
 
-    if model_selection == 'help':
-        print('This program is utilised to subtitle a video through the use of the OpenAI Whisper model.\n'
-              'To use this model, type [tiny/base/small/medium/large] to choose a model and initiate the process.\n'
-              'After this, you will need the file (mp3/flac/wav extensions accepted) and the language being '
-              'translated.\n')
-        time.sleep(3)
-        continue
+            while x:
+                file_path = input('Model selected. Insert file path: ')
 
-    elif model_selection == 'tiny' or model_selection == 'base' or model_selection == 'small' or model_selection == 'medium' or model_selection == 'large':
+                file_exists = os.path.isfile(file_path)
+                file_extension = os.path.splitext(file_path)[1]
 
-        x = True
-
-        while x:
-            file_path = input('Model selected. Insert file path: ')
-
-            file_exists = os.path.isfile(file_path)
-            file_extension = os.path.splitext(file_path)[1]
-
-            if not file_exists:
-                print('File does not exist. Please try again.')
-                continue
-            elif file_extension not in ['.mp3', '.flac', '.wav']:
-                print('File extension is not valid. Please try again.')
-                continue
-
-            elif file_exists and file_extension in ['.mp3', '.flac', '.wav']:
-                x = False
-
-        x = True
-
-        while x:
-            language = input('File found. Insert language code (can be found in languages.txt): ')
-
-            if language not in languages_accepted:
-                print('Language not valid. Please try again.')
-                continue
-
-            if language in languages_accepted:
-                print(f'Running Whisper with parameters: {model_selection}, {file_path}, {language}')
-                x = False
-
-        model = stable_whisper.load_faster_whisper(f'{model_selection}')
-        result = model.transcribe_stable(file_path, language=language)
-
-        result.to_srt_vtt('output.srt')
-
-        funct.srt_to_txt('output.srt', "plain_text_output.txt")
-        funct.txt_to_srt_clean('plain_text_output.txt', 'final_text_output.txt')
-        funct.remove_duplicate_lines('final_text_output.txt')
-
-        os.remove('plain_text_output.txt')
-
-        print('Converted to plaintext.')
-
-        translate_ask = input('Do you want to translate to English (TRANSLATION LONG AND ALMOST UNUSABLE)? (y/n): ')
-
-        if translate_ask == 'y':
-
-            while True:
-
-                translation_method = input('Do you want to translate through [google] translator? ')
-
-                if translation_method == 'google':
-
-                    translated = ''
-
-                    with open('final_text.txt', 'r', encoding='utf-8') as f:
-                        i = 1
-                        for line in f:
-                            u = deep_translator.GoogleTranslator(source='auto', target='en').translate(line)
-                            print(i)
-                            translated += f'{u}\n'
-                            i += 1
-
-                    with open('translated.txt', 'w', encoding='utf-8') as f:
-                        f.write(translated)
-
-                    break
-
-                else:
-                    print('Invalid input.')
+                if not file_exists:
+                    print('File does not exist. Please try again.')
+                    continue
+                elif file_extension not in ['.mp3', '.flac', '.wav']:
+                    print('File extension is not valid. Please try again.')
                     continue
 
+                elif file_exists and file_extension in ['.mp3', '.flac', '.wav']:
+                    x = False
 
-        else:
-            sys.exit()
+            x = True
 
-    else:
-        print('\nPlease select a valid option.\n')
-        continue
+            while x:
+                language = input('File found. Insert language code (can be found in languages.txt): ')
+
+                if language not in languages_accepted:
+                    print('Language not valid. Please try again.')
+                    continue
+
+                if language in languages_accepted:
+                    print(f'Running Whisper with parameters: {model_selection}, {file_path}, {language}')
+                    x = False
+
+            model = stable_whisper.load_faster_whisper(f'{model_selection}')
+            result = model.transcribe_stable(file_path, language=language)
+
+            result.to_srt_vtt('output.srt')
+
+            funct.srt_to_txt('output.srt', "plain_text_output.txt")
+            funct.txt_to_srt_clean('plain_text_output.txt', 'final_text_output.txt')
+            funct.remove_duplicate_lines('final_text_output.txt')
+
+            os.remove('plain_text_output.txt')
+
+            print('Converted to plaintext.')
+
+            translate_ask = input('Do you want to translate to English (TRANSLATION LONG AND ALMOST UNUSABLE)? (y/n): ')
+
+            if translate_ask == 'y':
+
+                while True:
+
+                    translation_method = input('Do you want to translate through [google] translator? ')
+
+                    if translation_method == 'google':
+
+                        translated = ''
+
+                        with open('final_text.txt', 'r', encoding='utf-8') as f:
+                            i = 1
+                            for line in f:
+                                u = deep_translator.GoogleTranslator(source='auto', target='en').translate(line)
+                                print(i)
+                                translated += f'{u}\n'
+                                i += 1
+
+                        with open('translated.txt', 'w', encoding='utf-8') as f:
+                            f.write(translated)
+
+                        break
+
+                    else:
+                        print('Invalid input.')
+                        continue
+
+
+            else:
+                sys.exit()
+
+        case _ :
+            print('\nPlease select a valid option.\n')
