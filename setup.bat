68ce80d4cd92ccd865f4684473b5b66f4a029a8e
pmavro <50794896+pmavro@users.noreply.github.com>
2024-01-01 23:36:24 +1100

    added setup key input

--- setup.bat
+++ setup.bat
@@ -1,4 +1,17 @@
+@echo off
+
 python -m pip install -r requirements.txt
 python -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
+if not exist settings mkdir settings
+
+set /p CHATGPT=What is your OpenAI GPT API key (leave blank if none):
+echo GPT key is : %CHATGPT%
+echo CHATGPT=%CHATGPT% > settings\keys.txt
+
+set /p MICROSOFT=What is your Microsoft Translator key (leave blank if none): 
+echo Microsoft key is: %MICROSOFT%
+echo MICROSOFT=%MICROSOFT% >> settings\keys.txt
+
 echo Completed.
 timeout /t 3
